b9c389682abed11405d04eae22f75e918c9067fc
Yurii Kosiv <41447717+yurdosii@users.noreply.github.com>
2023-05-08 13:04:10 +0300

    feat: setup onboarding logic (#3)

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import Response
+
+from .models import Survey, SurveyCreate
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://0.0.0.0:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# MVP solution for right now, later some SQL/NoSQL DBs can be used
+RESULTS: list[Survey] = []
 
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/surveys/")
+async def add_survey_result(data: SurveyCreate):
+    survey = Survey(**data.dict())
+    RESULTS.append(survey)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.get("/surveys/")
+async def get_survey_results() -> list[Survey]:
+    return RESULTS
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,13 +1,12 @@
 "use client"
 
-import Button from '@mui/material/Button';
+import Onboarding from "./onboarding";
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      Hello
-      <Button variant="contained"> Hello World</Button>
+      <Onboarding />
     </main>
   )
 }
