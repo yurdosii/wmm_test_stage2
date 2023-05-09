f60b7af0acfc8cfdc21e73a6acfb1d54c3fb04f7
Yurii Kosiv <yurdosii.ksv@gmail.com>
2023-05-09 10:09:04 +0300

    feat: prepare for deployment
    - added .env file / logic to frontend
    - added .env file / logic to backend
    - added link to "/results" on the onboarding finish
    - added files for deployment (vercel.json, requirements.txt)
    - updated models.py for deployment ("str | None" -> "Optional[str]")

---
+++ backend/src/wmm_backend/settings.py
@@ -0,0 +1,7 @@
+from environs import Env
+
+env = Env()
+env.read_env()
+
+FRONTEND_URL = env.str("FRONTEND_URL", "http://localhost:3000")
+CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", FRONTEND_URL)
